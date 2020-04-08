7a31abb8519c6905ae2003377f1d95af7d9a656e
dnereutskii <dmitriy.93.23@gmail.com>
2020-04-08 16:50:08 +0300

    Digital inverter was added

---
+++ inv/inv.vhd
@@ -0,0 +1,12 @@
+library IEEE; use IEEE.STD_LOGIC_1164.all;
+
+entity inv is
+	port(a: in STD_LOGIC_VECTOR(3 downto 0);
+		 y: out STD_LOGIC_VECTOR(3 downto 0));
+end;
+
+architecture synch of inv is 
+begin
+	y <= not a;
+end;
+
